d8851d54d86d3ba6e44cf068fff8f47e14ac807e
Raja <rajalingam210901@gmail.com>
2023-10-29 12:51:43 +0530

    added desgin quize question

--- quize-app/src/quizefiles/quizesQuestions/htmlQuize.js
+++ quize-app/src/quizefiles/quizesQuestions/htmlQuize.js
@@ -1,16 +1,16 @@
 import React from "react";
-
+import  "./htmlQuize.css";
 
 class HtmlQuize extends React.Component{
 render(){
 
    return<div className="HtmlQuize">
 
-<label>1. What is Html?</label>
+<label className="question">1. What is Html?</label>
 
 <div className="questionFirst">
    <label>
-      <input type="radio" />
+      <input type="radio" className="option" />
       option1
    </label>
     <label>
@@ -25,14 +25,11 @@ render(){
       <input type="radio" />
       option1
    </label>
-   <label>
-      <input type="radio" />
-      option1
-   </label>
+  
   </div>
 
 {/* question box second */ }
-<label>2. what is html course?</label>
+<label className="question">2. what is html course?</label>
   <div className="questionSecond">
    <label>
       <input type="radio" />
@@ -50,14 +47,82 @@ render(){
       <input type="radio" />
       option1
    </label>
+  
+  </div>
+
+  <label className="question">3. what is html course?</label>
+  <div className="questionSecond">
    <label>
       <input type="radio" />
       option1
    </label>
+    <label>
+      <input type="radio" />
+      option1
+   </label>
+   <label>
+      <input type="radio" />
+      option1
+   </label>
+   <label>
+      <input type="radio" />
+      option1
+   </label>
+ 
   </div>
 
 
+  <label className="question">4.what is html course?</label>
+  <div className="questionSecond">
+   <label>
+      <input type="radio" />
+      option1
+   </label>
+    <label>
+      <input type="radio" />
+      option1
+   </label>
+   <label>
+      <input type="radio" />
+      option1
+   </label>
+   <label>
+      <input type="radio" />
+      option1
+   </label>
+   
+  </div>
+
+
+  <label className="question">5. what is html course?</label>
+  <div className="questionSecond">
+   <label>
+      <input type="radio" />
+      option1
+   </label>
+    <label>
+      <input type="radio" />
+      option1
+   </label>
+   <label>
+      <input type="radio" />
+      option1
+   </label>
+   <label>
+      <input type="radio" />
+      option1
+   </label>
+ 
+  </div>
+
+
+<button className="submitButton">Submit</button>    
     
+
+
+
+
+
    </div>
 }
 
